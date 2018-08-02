b81577f95e036ab7e96e391d6a8a60b78b850390
Shawn Sherwood <ssherwood@pivotal.io>
2018-08-02 11:13:06 -0400

    Adding metrics tile support to pipeline

---
+++ tasks/tiles/metrics/task.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+set -eu
+
+function isPopulated() {
+    local true=0
+    local false=1
+    local envVar="${1}"
+
+    if [[ "${envVar}" == "" ]]; then
+        return ${false}
+    elif [[ "${envVar}" == null ]]; then
+        return ${false}
+    else
+        return ${true}
+    fi
+}
+
+product_properties=$(
+  jq -n \
+    '
+    { }
+    '
+)
+
+product_network=$(
+  jq -n \
+    --arg network_name "$NETWORK_NAME" \
+    --arg other_azs "$DEPLOYMENT_NW_AZS" \
+    --arg singleton_az "$SINGLETON_JOB_AZ" \
+    '
+    {
+      "network": {
+        "name": $network_name
+      },
+      "service_network": {
+        "name": $network_name
+      },
+      "other_availability_zones": ($other_azs | split(",") | map({name: .})),
+      "singleton_availability_zone": {
+        "name": $singleton_az
+      }
+    }
+    '
+)
+
+product_resources=$(
+  jq -n \
+    '
+    { }
+    '
+)
+echo $product_properties
+echo $product_network
+echo $product_resources
+echo $product_properties >> configuration/product_properties
+echo $product_network >> configuration/product_network
+echo $product_resources >> configuration/product_resources
