1c7c87319b1c6909658ee29b7db20e451d5ed0e5
Shawn Sherwood <ssherwood@pivotal.io>
2018-08-06 13:07:37 -0400

    Adding PKS task

---
+++ tasks/tiles/pks/task.sh
@@ -0,0 +1,64 @@
+#!/bin/bash
+
+set -eu
+
+function isPopulated() {
+    local true=0
+    local false=1
+    local envVar="${1}"
+
+    if [[ "${envVar}" == "" ]]; then
+        return ${false}
+    elif [[ "${envVar}" == null ]]; then
+        return ${false}
+    else
+        return ${true}
+    fi
+}
+
+product_properties=$(
+  jq -n \
+    --arg azs "$DEPLOYMENT_NW_AZS" \
+    --arg auth_json "$GCP_SERVICE_ACCOUNT_KEY" \
+    '
+    {
+      ".properties.pks_api_hostname": { "value": "pks.pcf.pcfplatform.space" },
+      ".properties.cloud_provider": { "value": "GCS" },
+    }
+    '
+)
+
+product_network=$(
+  jq -n \
+    --arg network_name "$NETWORK_NAME" \
+    --arg other_azs "$DEPLOYMENT_NW_AZS" \
+    --arg singleton_az "$SINGLETON_JOB_AZ" \
+    '
+    {
+      "network": {
+        "name": $network_name
+      },
+      "service_network": {
+        "name": $network_name
+      },
+      "other_availability_zones": ($other_azs | split(",") | map({name: .})),
+      "singleton_availability_zone": {
+        "name": $singleton_az
+      }
+    }
+    '
+)
+
+product_resources=$(
+  jq -n \
+    '
+    { }
+    '
+)
+
+echo $product_properties
+echo $product_network
+echo $product_resources
+echo $product_properties >> configuration/product_properties
+echo $product_network >> configuration/product_network
+echo $product_resources >> configuration/product_resources
